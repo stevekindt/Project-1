d71dafe4ec120d23cf0f17f115b5db6f3b2b20cd
Jack Fang <fang4268@stthomas.edu>
2020-01-30 21:17:49 -0600

    adding searching magnifying glass button. adding script. changed place holder

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
 //LANDING PAGE
-//1. BUTTON WITH ONCLICK EVENT; TAKES USER TO SECOND PAGE; 
-
-
-
-
-
-
-
-
+//1. BUTTON WITH ONCLICK EVENT; TAKES USER TO SECOND PAGE;
+$("#searchButton").on("click", function() {
+  var city;
+  city = $("#searchArea").val();
+  localStorage.chosencity = city;
+  location.href = "second-page.html";
+});
 
 //2. SEARCH AREA; CAPTURE USER INPUT; ADD INPUT TO LOCAL STORAGE
-
-
-
-
-
-
-
-
 
 //OPERATIONAL PAGE
 //1. WEATHER API
 
-
-
-
-
-
-
-
-
 //2. AMADEUS: POINTS OF INTEREST
 
-
-
-
-
-
-
-
-
 //3. AMADEUS: HOTELS
-
-
-
-
-
-
-
-
-
