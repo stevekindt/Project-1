5337eaa684006b62b9a8421e1abc780ca0cea6ec
Tommy Boone <tommyboone23@guest-10-166-211-170.wireless.umn.edu>
2020-01-30 21:22:06 -0600

    started ajax call for weather API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //LANDING PAGE
 //1. BUTTON WITH ONCLICK EVENT; TAKES USER TO SECOND PAGE; 
 
@@ -21,6 +22,20 @@
 
 //OPERATIONAL PAGE
 //1. WEATHER API
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=49b107df79df951ca90870bc8b2042c1";
+var city = $("#searchArea").val();
+
+$("#enterCity").on('click',function(event){
+    event.preventDefault();
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
+});
+
 
 
 
